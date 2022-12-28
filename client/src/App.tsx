b2b4ea7a99d65a781ff9751f4d810295a0c9a7f3
oneny <gjwodnjs1996@gmail.com>
2022-12-28 15:06:23 +0900

    feat: 리덕스 초기 세팅 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
+import { Provider } from 'react-redux';
 import Router from 'routes/Router';
+import { store } from 'store';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/globalStyle';
 import theme from 'styles/theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
---
+++ client/src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './apis/apiSlice';
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(),
+  devTools: true,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
